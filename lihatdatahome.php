cf5736f46146b5056ad59c91eabb780893395645
151610174-smkassalaambandung <151610174@smkassalaambandung.sch.id>
2017-10-24 15:59:23 +0700

    Ali Ilham

---
+++ lihatdatahome.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="/ali/home/pp.jpg">
+    <title>website sekolah</title>
+<!-- Bootstrap core CSS -->
+     <link href="c/1.css" rel="stylesheet">
+    <link href="/utsali/BS/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="/utsali/BS/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/utsali/BS/dist/css/bootstrap-theme.css" rel="stylesheet">
+    <link href="/utsali/BS/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="offcanvas.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="BS/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="BS/assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    <style>
+        body {
+            font-family: 'Lato';
+        }
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
+</head>
+<body id="app-layout">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="/ali/BS/dist/css/bootstrap.css" rel="stylesheet">
+                    website sekolah
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-left">
+                    <li><a href="/ulangan/home/lihatdatahome.php">Home</a></li>
+                    
+                </ul>
+
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                                                                    <!-- <li><a href="http://server.app/uts-xi/register">Register</a></li> -->
+                       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">website sekolah</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+           <li><a href="/utsali/home/lihatdatahome.php">home</a></li>
+            <li><a href="/utsali/pendaftar/lihatdatapendaftar.php">pendaftaran</a></li>
+            <li><a href="/utsali/jurusan/lihatdatajurusan.php">jurusan</a></li>
+            <li><a href="/utsali/penguji/lihatdatapenguji.php">penguji</a></li>
+            <li><a href="/utsali/kepalasekolah/lihatdatakepalasekolah.php">kepala sekolah</a></li>
+            <li><a href="/utsali/hasil/lihatdatahasil.php">hasil</a></li>
+           <li><a href="/utsali/pengumuman/lihatdatapengumuman.php">pengumuman</a></li>
+
+                                                           </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <dir class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <h3 class="text-center">
+                            selamat mendaftar
+                        </h3>
+                        <p class="text-center">
+                        </p>
+                        <p class="text-center">
+                            selamat datang diwebsite saya silahkan anda malakukan pendaftaran <br> 
+                            welcome. <br><br>
+                            <a href="/utsali/pendaftar/lihatdatapendaftar.php" class="btn btn-success">pendaftaran</a>
+                        </p>
+                    </div>
+                </div>
+            </dir>
+        </div>
+    </div>
+</div>
+
+    
+    </body>
+</html>
